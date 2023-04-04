d8b318b01e8e660f453325f6f0c5ce6a0d061826
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-04-03 20:25:08 -0500

    put materialize in the html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var requestUrl = 'https://api.seatgeek.com/2/events?client_id=MzI3NDA1MDJ8MTY4MDIxOTA5Ni41MTQxMzg3';
+var requestUrl = "https://api.seatgeek.com/2/events?client_id=" + ApiKey;
+var ApiKey = "MzI3NDA1MDJ8MTY4MDIxOTA5Ni41MTQxMzg3";
 
 fetch(requestUrl)
   .then(function (response) {
-
     return response.json();
   })
   .then(function (data) {
-    console.log('Fetch Response \n-------------');
+    console.log("Fetch Response \n-------------");
     console.log(data);
   });
